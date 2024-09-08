c16fbfa690bfe9c363cfe4cab5f7a2875ccb60ac
R-Kiselev <sputnavik@gmail.com>
2024-09-09 00:37:32 +0300

    menu in main

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -42,12 +42,33 @@ int main()
             break;
         case menu_options::add_card:
         {
-            handle_add_card(account);
+            std::string card_number;
+            std::string expire_date;
+
+            int card_balance;
+            std::cout << "Enter card number: ";
+            std::cin >> card_number;
+            std::cout << "Enter expire date (MM/YY): ";
+            std::cin >> expire_date;
+            std::cout << "Enter card balance: ";
+            std::cin >> card_balance;
+            account->add_bank_card(std::make_unique<BankCard>(card_number, expire_date, card_balance));
             break;
         }
         case menu_options::delete_card:
         {
-            handle_delete_card(account);
+            std::string card_number;
+            std::cout << "Enter card number to delete: ";
+            std::cin >> card_number;
+
+            if (account->delete_bank_card(card_number))
+            {
+                std::cout << "Card was deleted" << std::endl;
+            }
+            else
+            {
+                std::cout << "Card was not found" << std::endl;
+            }
 
             break;
         }
@@ -56,12 +77,18 @@ int main()
             break;
         case menu_options::set_name:
         {
-            handle_set_name(account);
+            std::string new_name;
+            std::cout << "Enter new account name: ";
+            std::cin >> new_name;
+            account->set_client_name(new_name);
             break;
         }
         case menu_options::set_balance:
         {
-            handle_set_balance(account);
+            int new_balance;
+            std::cout << "Enter new account balance: ";
+            std::cin >> new_balance;
+            account->set_account_balance(new_balance);
             break;
         }
         case menu_options::end:
@@ -71,51 +98,4 @@ int main()
             std::cout << "Invalid option. Please try again" << std::endl;
         }
     }
-}
-
-void handle_set_balance(std::unique_ptr<Account, std::default_delete<Account>>& account)
-{
-    int new_balance;
-    std::cout << "Enter new account balance: ";
-    std::cin >> new_balance;
-    account->set_account_balance(new_balance);
-}
-
-void handle_set_name(std::unique_ptr<Account, std::default_delete<Account>>& account)
-{
-    std::string new_name;
-    std::cout << "Enter new account name: ";
-    std::cin >> new_name;
-    account->set_client_name(new_name);
-}
-
-void handle_delete_card(std::unique_ptr<Account, std::default_delete<Account>>& account)
-{
-    std::string card_number;
-    std::cout << "Enter card number to delete: ";
-    std::cin >> card_number;
-
-    if (account->delete_bank_card(card_number))
-    {
-        std::cout << "Card was deleted" << std::endl;
-    }
-    else
-    {
-        std::cout << "Card was not found" << std::endl;
-    }
-}
-
-void handle_add_card(std::unique_ptr<Account, std::default_delete<Account>>& account)
-{
-    std::string card_number;
-    std::string expire_date;
-
-    int card_balance;
-    std::cout << "Enter card number: ";
-    std::cin >> card_number;
-    std::cout << "Enter expire date (MM/YY): ";
-    std::cin >> expire_date;
-    std::cout << "Enter card balance: ";
-    std::cin >> card_balance;
-    account->add_bank_card(std::make_unique<BankCard>(card_number, expire_date, card_balance));
 }
--- Lab1/main.h
+++ Lab1/main.h
@@ -1,7 +1,1 @@
 void handle_add_card(std::unique_ptr<Account, std::default_delete<Account>>& account);
-
-void handle_delete_card(std::unique_ptr<Account, std::default_delete<Account>>& account);
-
-void handle_set_name(std::unique_ptr<Account, std::default_delete<Account>>& account);
-
-void handle_set_balance(std::unique_ptr<Account, std::default_delete<Account>>& account);
