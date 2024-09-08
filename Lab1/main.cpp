365e53a69133d0855c2036371f177e530c112e5f
R-Kiselev <sputnavik@gmail.com>
2024-09-08 19:59:52 +0300

    Added BankCard class

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,5 +1,59 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-	std::cout << "Hello world";
+class BankCard
+{
+private:
+	std::string CardNumber_;
+	std::string ExpireDate_;
+	int CardBalance_;
+public:
+	BankCard(std::string CardNumber, std::string ExpireDate, int CardBalance = 0)
+		: CardNumber_(CardNumber), ExpireDate_(ExpireDate), CardBalance_(CardBalance)
+	{
+	}
+
+	void SetCardNumber(std::string CardNumber)
+	{
+		CardNumber_ = CardNumber;
+	}
+
+	void SetExpireDate(std::string ExpireDate)
+	{
+		ExpireDate_ = ExpireDate;
+	}
+
+	void SetCardBalance(int CardBalance)
+	{
+		CardBalance_ = CardBalance;
+	}
+
+	std::string GetCardNumber()
+	{
+		return CardNumber_;
+	}
+
+	std::string GetExpireDate()
+	{
+		return ExpireDate_;
+	}
+
+	int GetCardBalance()
+	{
+		return CardBalance_;
+	}
+
+	void DisplayCardInfo()
+	{
+		std::cout << "Card number:" << CardNumber_ << std::endl;
+		std::cout << "Expire date:" << ExpireDate_ << std::endl;
+		std::cout << "Card balance:" << CardBalance_ << std::endl;
+	}
+};
+
+
+int main()
+{
+	
 }
