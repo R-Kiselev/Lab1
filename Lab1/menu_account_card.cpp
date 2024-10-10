58e1d1fa64479271e6d81852dc621ae0caa6b4de
R-Kiselev <sputnavik@gmail.com>
2024-10-11 01:00:17 +0300

    Divided the menu by functions

--- Lab1/menu_account_card.cpp
+++ Lab1/menu_account_card.cpp
@@ -3,30 +3,11 @@
 void menu_account_card(sqlite3* DB)
 {
     AccountCollection ac;
-
     ac.load_collection_from_db(DB);
 
     while (true)
     {
-        std::cout << "1) Display accounts info" << std::endl;
-        std::cout << "2) Add account" << std::endl;
-        std::cout << "3) Delete account" << std::endl;
-
-        std::cout << "4) Add card" << std::endl;
-        std::cout << "5) Delete card" << std::endl;
-        
-        std::cout << "6) Set account name" << std::endl;
-        std::cout << "7) Set account balance" << std::endl;
-        
-        std::cout << "8) Transfer money between account and card" << std::endl;
-        std::cout << "9) Transfer money between two cards" << std::endl;
-
-        std::cout << "10) Test assignment operator" << std::endl;
-        std::cout << "11) Test equality operator" << std::endl;
-
-        std::cout << "0) Exit" << std::endl;
-        std::cout << "Your option : ";
-
+        display_menu();
         int choice;
         std::cin >> choice;
         std::cout << std::endl;
@@ -35,214 +16,252 @@ void menu_account_card(sqlite3* DB)
         switch (option)
         {
         case menu_options::accounts_info:
-            ac.print_accounts();
+            handle_accounts_info(ac);
             break;
         case menu_options::add_account:
-        {
-            int id;
-            int balance;
-            std::string client_name;
-
-            std::cout << "Enter id: ";
-            std::cin >> id;
-            std::cout << "Enter client name: ";
-            std::cin >> client_name;
-            std::cout << "Enter balance: ";
-            std::cin >> balance;
-
-            ac.create_account(id, client_name, balance);
-            ac[id]->save_to_db(DB);
+            handle_add_account(ac, DB);
             break;
-        }
         case menu_options::delete_account:
-        {
-            int id;
-            std::cout << "Enter id of account to delete: ";
-            std::cin >> id;
-
-            if (ac[id])
-            {
-                ac[id]->delete_from_db(DB);
-                ac.delete_account(id);
-            }
-            else
-            {
-                std::cerr << "There is no account with ID= " << id << std::endl;
-            }
+            handle_delete_account(ac, DB);
             break;
-        }
         case menu_options::add_card:
-        {
-            int id;
-            std::string card_number;
-            std::string expire_date;
-            int card_balance;
-            int account_id;
-
-            std::cout << "Enter card id: ";
-            std::cin >> id;
-            std::cout << "Enter card number: ";
-            std::cin >> card_number;
-            std::cout << "Enter expire date (MM/YY): ";
-            std::cin >> expire_date;
-            std::cout << "Enter card balance: ";
-            std::cin >> card_balance;
-
-            std::cout << "Enter account id: ";
-            std::cin >> account_id;
-
-            try
-            {
-                if (find_card(ac, id))
-                    throw CardException(std::format("Card with ID = {} already exists", id));
-
-                if (!ac[account_id])
-                    throw CardException(std::format("There is no account with ID = {}", account_id));
-                
-                auto card = std::make_unique<Card>(id, card_number, expire_date, card_balance, account_id);
-                card->save_to_db(DB);
-                ac[account_id]->add_card(std::move(card));
-            }
-            catch (const CardException& e)
-            {
-                std::cerr << e.what() << std::endl << std::endl;
-            }
+            handle_add_card(ac, DB);
             break;
-        }
         case menu_options::delete_card:
-        {
-            int account_id;
-            int card_id;
-
-            std::cout << "Enter the ID of the account that the card belongs to: ";
-            std::cin >> account_id;
-            std::cout << "Enter card id to delete: ";
-            std::cin >> card_id;
-
-            if (auto card = find_card(ac, card_id))
-            {
-                card->delete_from_db(DB);
-                ac[account_id]->delete_card(card_id);
-                std::cout << "Card was deleted" << std::endl << std::endl;
-            }
-            else
-            {
-                std::cout << "Card was not found" << std::endl << std::endl;
-            }
+            handle_delete_card(ac, DB);
             break;
-        }
         case menu_options::set_name:
-        {
-            int account_id;
-            std::string name;
-            std::cout << "Enter account id: ";
-            std::cin >> account_id;
-            std::cout << "Enter new name: ";
-            std::cin >> name;
-
-            auto account = ac.find_account_by_id(account_id);
-            account->set_name(name);
-
-            ac.save_collection_to_db(DB);
+            handle_set_name(ac, DB);
             break;
-        }
         case menu_options::set_balance:
-        {
-            int account_id;
-            int balance;
-            std::cout << "Enter account id: ";
-            std::cin >> account_id;
-            std::cout << "Enter new balance: ";
-            std::cin >> balance;
-
-            auto account = ac.find_account_by_id(account_id);
-            account->set_balance(balance);
-            ac.save_collection_to_db(DB);
+            handle_set_balance(ac, DB);
             break;
-        }
         case menu_options::transfer_account:
-        {
-            int account_id;
-            std::string card_number;
-            int sum;
-
-            std::cout << "Enter sender's account id: ";
-            std::cin >> account_id;
-            std::cout << "Enter recipient card number: ";
-            std::cin >> card_number;
-            std::cout << "Enter sum: ";
-            std::cin >> sum;
-
-            ac.transfer_money_to_account_card(account_id, card_number, sum);
-            ac.save_collection_to_db(DB);
+            handle_transfer_account(ac, DB);
             break;
-        }
         case menu_options::transfer_cards:
-        {
-            std::string recipient_card;
-            std::string sender_card;
-            int sum;
-
-            std::cout << "Enter recipient card number: ";
-            std::cin >> recipient_card;
-            std::cout << "Enter sender card number: ";
-            std::cin >> sender_card;
-            std::cout << "Enter sum: ";
-            std::cin >> sum;
-
-            ac.transfer_money_between_cards(recipient_card, sender_card, sum);
-            ac.save_collection_to_db(DB);
+            handle_transfer_cards(ac, DB);
             break;
-        }
         case menu_options::test_assignment_operator:
-        {
-            std::string source_card;
-            std::string destination_card;
-
-            std::cout << "Enter first card ID to copy from: ";
-            std::cin >> source_card;
-            std::cout << "Enter second card ID to copy to: ";
-            std::cin >> destination_card;
-
-            if (const Card* card_from = find_card(ac, source_card);
-                Card* card_to = find_card(ac, destination_card)) {
-                *card_to = *card_from;
-                std::cout << "Card " << destination_card << " assigned the values from card " << source_card << std::endl;
-            }
-            else {
-                std::cout << "One of the cards not found." << std::endl;
-            }
-            ac.save_collection_to_db(DB);
+            handle_test_assignment_operator(ac);
             break;
-        }
         case menu_options::test_equality_operator:
-        {
-            std::string source_card;
-            std::string destination_card;
-
-            std::cout << "Enter first card number to compare with: ";
-            std::cin >> source_card;
-            std::cout << "Enter second card number to compare with: ";
-            std::cin >> destination_card;
-
-            if (const Card* card_from = find_card(ac, source_card);
-                const Card* card_to = find_card(ac, destination_card)) {
-                (*card_from == *card_to) ?
-                    std::cout << "Cards " << source_card << " and " << destination_card << " are equal in balance and expire date." << std::endl
-                :
-                    std::cout << "Cards " << source_card << " and " << destination_card << " are not equal." << std::endl;
-                
-            }
-            else {
-                std::cout << "One of the cards not found." << std::endl;
-            }
+            handle_test_equality_operator(ac);
             break;
-        }
         case menu_options::end:
             std::cout << "Exit" << std::endl;
             return;
         default:
             std::cout << "Invalid option. Please try again" << std::endl;
         }
+    }
+}
+
+void display_menu()
+{
+    std::cout << "1) Display accounts info" << std::endl;
+    std::cout << "2) Add account" << std::endl;
+    std::cout << "3) Delete account" << std::endl;
+    std::cout << "4) Add card" << std::endl;
+    std::cout << "5) Delete card" << std::endl;
+    std::cout << "6) Set account name" << std::endl;
+    std::cout << "7) Set account balance" << std::endl;
+    std::cout << "8) Transfer money between account and card" << std::endl;
+    std::cout << "9) Transfer money between two cards" << std::endl;
+    std::cout << "10) Test assignment operator" << std::endl;
+    std::cout << "11) Test equality operator" << std::endl;
+    std::cout << "0) Exit" << std::endl;
+    std::cout << "Your option: ";
+}
+
+void handle_accounts_info(AccountCollection& ac)
+{
+    ac.print_accounts();
+}
+
+void handle_add_account(AccountCollection& ac, sqlite3* DB)
+{
+    int id, balance;
+    std::string client_name;
+
+    std::cout << "Enter id: ";
+    std::cin >> id;
+    std::cout << "Enter client name: ";
+    std::cin >> client_name;
+    std::cout << "Enter balance: ";
+    std::cin >> balance;
+
+    ac.create_account(id, client_name, balance);
+    ac[id]->save_to_db(DB);
+}
+
+void handle_delete_account(AccountCollection& ac, sqlite3* DB)
+{
+    int id;
+    std::cout << "Enter id of account to delete: ";
+    std::cin >> id;
+
+    if (ac[id])
+    {
+        ac[id]->delete_from_db(DB);
+        ac.delete_account(id);
+    }
+    else
+    {
+        std::cerr << "There is no account with ID= " << id << std::endl;
+    }
+}
+
+void handle_add_card(AccountCollection& ac, sqlite3* DB)
+{
+    int id, card_balance, account_id;
+    std::string card_number, expire_date;
+
+    std::cout << "Enter card id: ";
+    std::cin >> id;
+    std::cout << "Enter card number: ";
+    std::cin >> card_number;
+    std::cout << "Enter expire date (MM/YY): ";
+    std::cin >> expire_date;
+    std::cout << "Enter card balance: ";
+    std::cin >> card_balance;
+    std::cout << "Enter account id: ";
+    std::cin >> account_id;
+
+    try
+    {
+        if (find_card(ac, id))
+            throw CardException(std::format("Card with ID = {} already exists", id));
+        if (!ac[account_id])
+            throw CardException(std::format("There is no account with ID = {}", account_id));
+
+        auto card = std::make_unique<Card>(id, card_number, expire_date, card_balance, account_id);
+        card->save_to_db(DB);
+        ac[account_id]->add_card(std::move(card));
+    }
+    catch (const CardException& e)
+    {
+        std::cerr << e.what() << std::endl << std::endl;
+    }
+}
+
+void handle_delete_card(AccountCollection& ac, sqlite3* DB)
+{
+    int account_id, card_id;
+
+    std::cout << "Enter the ID of the account that the card belongs to: ";
+    std::cin >> account_id;
+    std::cout << "Enter card id to delete: ";
+    std::cin >> card_id;
+
+    if (auto card = find_card(ac, card_id))
+    {
+        card->delete_from_db(DB);
+        ac[account_id]->delete_card(card_id);
+        std::cout << "Card was deleted" << std::endl << std::endl;
+    }
+    else
+    {
+        std::cout << "Card was not found" << std::endl << std::endl;
+    }
+}
+
+void handle_set_name(AccountCollection& ac, sqlite3* DB)
+{
+    int account_id;
+    std::string name;
+    std::cout << "Enter account id: ";
+    std::cin >> account_id;
+    std::cout << "Enter new name: ";
+    std::cin >> name;
+
+    auto account = ac.find_account_by_id(account_id);
+    account->set_name(name);
+    ac.save_collection_to_db(DB);
+}
+
+void handle_set_balance(AccountCollection& ac, sqlite3* DB)
+{
+    int account_id, balance;
+    std::cout << "Enter account id: ";
+    std::cin >> account_id;
+    std::cout << "Enter new balance: ";
+    std::cin >> balance;
+
+    auto account = ac.find_account_by_id(account_id);
+    account->set_balance(balance);
+    ac.save_collection_to_db(DB);
+}
+
+void handle_transfer_account(AccountCollection& ac, sqlite3* DB)
+{
+    int account_id, sum;
+    std::string card_number;
+
+    std::cout << "Enter sender's account id: ";
+    std::cin >> account_id;
+    std::cout << "Enter recipient card number: ";
+    std::cin >> card_number;
+    std::cout << "Enter sum: ";
+    std::cin >> sum;
+
+    ac.transfer_money_to_account_card(account_id, card_number, sum);
+    ac.save_collection_to_db(DB);
+}
+
+void handle_transfer_cards(AccountCollection& ac, sqlite3* DB)
+{
+    std::string recipient_card, sender_card;
+    int sum;
+
+    std::cout << "Enter recipient card number: ";
+    std::cin >> recipient_card;
+    std::cout << "Enter sender card number: ";
+    std::cin >> sender_card;
+    std::cout << "Enter sum: ";
+    std::cin >> sum;
+
+    ac.transfer_money_between_cards(recipient_card, sender_card, sum);
+    ac.save_collection_to_db(DB);
+}
+
+void handle_test_assignment_operator(AccountCollection& ac)
+{
+    std::string source_card, destination_card;
+
+    std::cout << "Enter first card ID to copy from: ";
+    std::cin >> source_card;
+    std::cout << "Enter second card ID to copy to: ";
+    std::cin >> destination_card;
+
+    if (const Card* card_from = find_card(ac, source_card);
+        Card * card_to = find_card(ac, destination_card))
+    {
+        *card_to = *card_from;
+        std::cout << "Card " << destination_card << " assigned the values from card " << source_card << std::endl;
+    }
+    else
+    {
+        std::cout << "One of the cards not found." << std::endl;
+    }
+}
+
+void handle_test_equality_operator(AccountCollection& ac)
+{
+    std::string source_card, destination_card;
+
+    std::cout << "Enter first card number to compare with: ";
+    std::cin >> source_card;
+    std::cout << "Enter second card number to compare with: ";
+    std::cin >> destination_card;
+
+    if (const Card* card_from = find_card(ac, source_card);
+        const Card * card_to = find_card(ac, destination_card))
+    {
+        std::cout << "Cards are " << ((*card_from == *card_to) ? "equal" : "not equal") << std::endl;
+    }
+    else
+    {
+        std::cout << "One of the cards not found." << std::endl;
     }
 }
--- Lab1/menu_account_card.h
+++ Lab1/menu_account_card.h
@@ -32,3 +32,16 @@ enum class menu_options
 };
 
 void menu_account_card(sqlite3* DB);
+
+void display_menu();
+void handle_accounts_info(AccountCollection& ac);
+void handle_add_account(AccountCollection& ac, sqlite3* DB);
+void handle_delete_account(AccountCollection& ac, sqlite3* DB);
+void handle_add_card(AccountCollection& ac, sqlite3* DB);
+void handle_delete_card(AccountCollection& ac, sqlite3* DB);
+void handle_set_name(AccountCollection& ac, sqlite3* DB);
+void handle_set_balance(AccountCollection& ac, sqlite3* DB);
+void handle_transfer_account(AccountCollection& ac, sqlite3* DB);
+void handle_transfer_cards(AccountCollection& ac, sqlite3* DB);
+void handle_test_assignment_operator(AccountCollection& ac);
+void handle_test_equality_operator(AccountCollection& ac);
