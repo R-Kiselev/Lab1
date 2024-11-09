803333e774b843b06ecde0570e54730887442bc9
R-Kiselev <sputnavik@gmail.com>
2024-11-09 15:56:20 +0300

    refactor: changed project structure

--- qt/Widgets/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Widgets_FILE D:/Study/2/PHLL/qt/Widgets/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Widgets.exe)
-set(__QT_DEPLOY_TARGET_Widgets_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Widgets_RUNTIME_DLLS D:/Programming/QT/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Programming/QT/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Programming/QT/6.8.0/mingw_64/bin/Qt6Core.dll)
